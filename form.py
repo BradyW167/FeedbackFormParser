b91233c8a1e4cd749ec8f953e6df7b0fae9712da
Brady Werling <bradywerling@gmail.com>
2026-02-25 22:04:21 -0500

    Created abstract superclass for form objects

---
+++ form.py
@@ -0,0 +1,10 @@
+class Form:
+    def __init__(self, input_file_path):
+        self.input_file_path = input_file_path
+        self.data_frame = self._read_path()
+
+    def print_forms(self):
+        pass
+
+    def _read_path(self):
+        pass
